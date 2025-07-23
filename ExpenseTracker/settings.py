a3f21ac2d44c7ab0fc27c38af93c7f8f50cfa89b
Ayush <ayushgupta6593@gmail.com>
2025-07-23 23:53:04 +0530

    Fix: Correct project name casing

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Expense_Tracker.urls'
+ROOT_URLCONF = 'ExpenseTracker.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Expense_Tracker.wsgi.application'
+WSGI_APPLICATION = 'ExpenseTracker.wsgi.application'
 
 
 # Database
