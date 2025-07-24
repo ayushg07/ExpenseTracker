2c8fe0dadac846cb68598c9843125900f66d6a71
Ayush <ayushgupta6593@gmail.com>
2025-07-24 15:57:48 +0530

    Fix: Correct project name casing

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+SESSION_COOKIE_SECURE = True  # for HTTPS on Render
+CSRF_COOKIE_SECURE = True     # same for CSRF cookie
--- expenses/views.py
+++ expenses/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 
+
 def home(request):
     return render(request, 'expenses/home.html')
 
